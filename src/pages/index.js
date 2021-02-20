75c4944667a8bdbd8b65c73f875f35c11241b8aa
Bhajneet S.K <bhajneet@gmail.com>
2021-02-20 14:17:00 -0500

    feat: add getting started and featured articles to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,6 +39,52 @@ const products = [
   },
 ]
 
+const gettingStarted = [
+  {
+    title: 'Learn more about the Database',
+    description: 'At the heart of Shabad OS is an open source database containing the Sikh philosophy. Learning more about this philosophy and sharing it with others is our mission at Shabad OS.',
+    link: 'database',
+  },
+  {
+    title: 'Discover the Database online through your browser',
+    description: 'You can view the contents, translations, and other notes in the Database using our Viewer. In addition, each individual line has it\'s metre calculated. The Viewer may also be used to share discrepancies between the source material and digital data.',
+    link: 'viewer',
+  },
+  {
+    title: 'Installing the Presenter on Windows / macOS',
+    description: 'You can present the Database at gurdwaras, camps/retreats, and virtually through live captions on platforms like YouTube.',
+    link: 'tutorials/install',
+  },
+]
+
+const featuredArticles = [
+  {
+    title: 'Check out Gurmukhi Utils',
+    description: 'Gurmukhi Utils is a library for converting, analyzing, and testing gurmukhi strings.',
+    link: 'gurmukhi-utils',
+  },
+  {
+    title: 'Meet our community',
+    description: 'Ongoing development, project management, and marketing is made possible by the support and dedication of volunteers.',
+    link: 'community',
+  },
+  {
+    title: 'Get comfortable with contributing',
+    description: 'This document caters to developers or programmers that wish to write code and directly contribute to the source code of Shabad OS.',
+    link: 'community/how-to-contribute',
+  },
+  {
+    title: 'Learn about our coding guidelines',
+    description: 'We choose to use spaces, not tabs.',
+    link: 'community/coding-guidelines',
+  },
+  {
+    title: 'Get support & feedback',
+    description: 'Shabad OS is an open, community-driven, and volunteer project.',
+    link: 'community/support-and-feedback',
+  },
+]
+
 const Product = ( { title, description, link } ) => (
   <div className={clsx( 'col col--12', styles.feature )}>
     <Link to={link} className="product_card">
@@ -48,6 +94,19 @@ const Product = ( { title, description, link } ) => (
       </div>
       <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" className="svg-icon chevron-rounded-icon" viewBox="0 0 14 25"><path data-v-a76e93d8="" d="M1,24.4a.9.9,0,0,0,.7-.3L13.4,13a1,1,0,0,0,0-1.6L1.7.3A.9.9,0,0,0,1,0,.9.9,0,0,0,0,1a.9.9,0,0,0,.3.7l11,10.5L.3,22.7a.9.9,0,0,0-.3.7A.9.9,0,0,0,1,24.4Z" /></svg>
     </Link>
+  </div>
+)
+
+const ArticlesList = ( { title, description, link } ) => (
+  <div>
+    <p>
+      {description}
+      <br />
+      <Link to={link}>
+        {title}
+        {' >'}
+      </Link>
+    </p>
   </div>
 )
 
@@ -73,6 +132,16 @@ const Home = () => {
           <p className="lead">Explore our guides and articles by product.</p>
           <div className="row">
             {products.map( props => <Product key={props.title} {...props} /> ) }
+          </div>
+          <div className="row padding-vert--lg">
+            <div className="col col--6">
+              <h2>Getting Started</h2>
+              {gettingStarted.map( props => <ArticlesList key={props.title} {...props} /> ) }
+            </div>
+            <div className="col col--6">
+              <h2>Featured Articles</h2>
+              {featuredArticles.map( props => <ArticlesList key={props.title} {...props} /> ) }
+            </div>
           </div>
         </div>
       </main>
